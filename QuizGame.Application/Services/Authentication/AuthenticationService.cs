aa89a2c07ae88f985616e3efea31939c025d93ab
SirC00kie <kostomarov.2001@mail.ru>
2022-11-10 17:29:35 +0300

    аутентификация с исп. jwtToken

--- QuizGame.Application/Services/Authentication/AuthenticationService.cs
+++ QuizGame.Application/Services/Authentication/AuthenticationService.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using QuizGame.Application.Common.Interfaces.Authentication;
 
 namespace QuizGame.Application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
         public AuthenticationResult Register(string name, string email, string password)
         {
+            var userId = Guid.NewGuid();
+            
+            var token = _jwtTokenGenerator.GenerateToken(userId, name);
+            
             return new AuthenticationResult(
-                Guid.NewGuid(),
+                userId,
                 name,
                 email,
-                "token");
+                token);
         }
         
 
