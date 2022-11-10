92f1c419210ded978b59b7f5d2658b02b85bdd25
SirC00kie <kostomarov.2001@mail.ru>
2022-11-10 17:28:11 +0300

    генератор jwtToken

---
+++ QuizGame.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace QuizGame.Application.Common.Interfaces.Authentication
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(Guid userId, string name);
+    }
+}
--- QuizGame.Infrastructure/DependencyInjection.cs
+++ QuizGame.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QuizGame.Application.Common.Interfaces.Authentication;
+using QuizGame.Application.Common.Interfaces.Services;
+using QuizGame.Infrastructure.Authentication;
+using QuizGame.Infrastructure.Services;
 
 namespace QuizGame.Infrastructure
 {
     public static class DependencyInjections
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
         {
+            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             return services;
         }
     }
