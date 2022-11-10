073b3a8692edcaced6d34ccd9032606743c998a6
SirC00kie <kostomarov.2001@mail.ru>
2022-11-10 17:30:29 +0300

    jwtSettings

---
+++ QuizGame.Infrastructure/Authentication/JwtSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizGame.Infrastructure.Authentication;
+
+public class JwtSettings
+{
+    public const string SectionName = "JwtSettings";
+    
+    public string Secret { get; init; } = null!;
+
+    public int ExpireMinutes { get; init; }
+    
+    public string Issuer { get; init; } = null!;
+
+    public string Audience { get; init; } = null!;
+}
